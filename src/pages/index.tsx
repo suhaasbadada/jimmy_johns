3d7013eb1e083edf50dc0cd85cfcdc83c8bcdedd
moomurrs <59849596+moomurrs@users.noreply.github.com>
2024-11-17 14:13:32 -0500

    fix spelling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function IndexPage() {
                 <section className="border-l-8 border-gray-500 flex flex-col items-center justify-center bg-slate-100 rounded-r-3xl shadow-xl">
                     <h2 className="font-bold pb-2 text-lg">About the spills in Indiana</h2>
                     <Spacer y={3} />
-                    <h2>Spill per Year in Indiana</h2>
+                    <h2>Spills per Year in Indiana</h2>
                     <div className="h-80 w-full">
                         <SpillLine />
-                        <p className="text-center bg-white w-fit mx-auto m-2 px-1 rounded-md"><span className="italic">Figure 1:</span> This figure shows the spills reported in Indiana for each year from 2010 to 2014.</p>
+                        <p className="text-center bg-white w-fit mx-auto m-2 px-1 rounded-md"><span className="italic">Figure 1:</span> This figure shows the spills reported in Indiana for each year from 2010 to 2024.</p>
                     </div>
                 </section>
 
@@ -30,7 +30,7 @@ export default function IndexPage() {
                     <h2>Cumulative Toxins from 2010 - 2024</h2>
                     <div className="h-[54rem] w-full">
                         <CleanupBar />
-                        <p className="text-center bg-white w-fit m-2 px-1 mt-4 rounded-md mx-auto"><span className="italic">Figure 2:</span> This figure shows the detected toxins from 2010 to 2014.</p>
+                        <p className="text-center bg-white w-fit m-2 px-1 mt-4 rounded-md mx-auto"><span className="italic">Figure 2:</span> This figure shows the detected toxins from 2010 to 2024.</p>
                     </div>
                     <Spacer y={3} />
                 </section>
