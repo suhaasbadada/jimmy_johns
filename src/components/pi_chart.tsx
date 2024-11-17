f2597bcd1c08b324552e29119f60988005b74480
moomurrs <59849596+moomurrs@users.noreply.github.com>
2024-11-17 10:55:29 -0500

    add real pichart data

--- src/components/pi_chart.tsx
+++ src/components/pi_chart.tsx
@@ -1,10 +1,11 @@
 import { ResponsivePie } from '@nivo/pie';
-import dummy_pichart from "@/data/dummy_pichart.json";
+import data from "@/data/pie_chart_data.json"
+//import dummy_pichart from "@/data/dummy_pichart.json";
 
 export default function PiChart() {
     return (
         <ResponsivePie
-            data={dummy_pichart}
+            data={data}
             margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
             innerRadius={0.5}
             padAngle={0.7}
